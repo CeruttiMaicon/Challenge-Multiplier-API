440542b4f7bbac24dc7173123ccbf56673ab98a0
Maicon Cerutti <maicon.cerutti@facedigital.com.br>
2020-10-03 19:02:54 -0300

    adicionando documentacao

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    //theme: "yuu",
+    title: "Desafio Multiplier",
+    description: "Documentação do Desafio",
+    themeConfig: {
+        // yuu: {
+        //   disableDarkTheme: false,
+        // },
+        smoothScroll: true,
+        logo: "/images/logo-dark.png",
+        nav: [{
+                text: "Requisitos",
+                link: "/requisitos/",
+            },
+            {
+                text: "API",
+                link: "/api/",
+            },
+        ],
+        sidebar: {
+            "/requisitos/": ["", "usuarios", "categorias", "produtos", "pedidos"],
+            "/api/": ["", "usuarios", "categorias", "produtos", "pedidos"],
+        },
+    },
+};
