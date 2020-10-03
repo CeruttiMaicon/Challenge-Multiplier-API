9289e5dd8bafa2ae6379aab151d6e04a3aa64e69
Maicon Cerutti <maicon.cerutti@facedigital.com.br>
2020-10-03 18:01:33 -0300

    adicionado seeder de criação do usuário

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'name' => 'Maicon Cerutti',
+            'email' => 'maicon@gmail.com',
+            'password' => Hash::make('secret')
+        ]);
+    }
+}
